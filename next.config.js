ad161bef483183a1c1df2b3f5c95b9ce6613a68c
rbkayz <62215539+rbkayz@users.noreply.github.com>
2023-06-29 21:01:20 +0530

    fix: updated config

--- next.config.js
+++ next.config.js
@@ -1,38 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['localhost'],
-      // loader: 'custom',
-      // loaderFile: './app/_utils/imageLoader.ts'
-    },
-    // async headers() {
-    //   return [
-    //     {
-    //       source: '/login',
-    //       headers: [
-    //         {
-    //           key: 'Content-Security-Policy',
-    //           value: 'script-src https://accounts.google.com/gsi/client; frame-src https://accounts.google.com/gsi/; connect-src https://accounts.google.com/gsi/;',
-    //         },
-    //       ],
-    //     },
-    //   ]
-    // },
-    async redirects() {
-      return [
-        {
-          source: '/documents/:slug',
-          destination: '/documents/:slug/links', // Matched parameters can be used in the destination
-          permanent: true,
-        },
-        {
-          source: '/settings',
-          destination: '/settings/profile', // Matched parameters can be used in the destination
-          permanent: true,
-        },
-      ]
-    },
-  }
+  reactStrictMode: true,
+  images: {
+    domains: ["localhost", "https://dblpeefwccpldqwuzwza.supabase.co"],
+    // loader: 'custom',
+    // loaderFile: './app/_utils/imageLoader.ts'
+  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/login',
+  //       headers: [
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: 'script-src https://accounts.google.com/gsi/client; frame-src https://accounts.google.com/gsi/; connect-src https://accounts.google.com/gsi/;',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
+  async redirects() {
+    return [
+      {
+        source: "/documents/:slug",
+        destination: "/documents/:slug/links", // Matched parameters can be used in the destination
+        permanent: true,
+      },
+      {
+        source: "/settings",
+        destination: "/settings/profile", // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
