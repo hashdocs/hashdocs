8db20f09f98c13183ca3dadd613c6037730dc165
rbkayz <62215539+rbkayz@users.noreply.github.com>
2023-07-02 20:57:52 +0530

    chore: updated types

---
+++ supabase/migrations/20230702152658_function_updates.sql
@@ -0,0 +1,219 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_documents(document_id_input text DEFAULT NULL::text)
+ RETURNS json
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+	return_data json;
+BEGIN
+	WITH views AS (
+		SELECT
+			tbl_views.view_seq,
+			tbl_views.link_id,
+			tbl_views.view_id,
+			tbl_views.viewed_at,
+			tbl_views.viewer,
+			tbl_views.document_version,
+			coalesce(sum(tbl_view_logs.end_time - tbl_view_logs.start_time), 0) AS duration,
+			round((count(DISTINCT page_num)::float / page_count * 100)::numeric) AS completion
+		FROM
+			tbl_views
+		LEFT JOIN tbl_view_logs ON tbl_views.view_id = tbl_view_logs.view_id
+		LEFT JOIN tbl_links ON tbl_links.link_id = tbl_views.link_id
+		LEFT JOIN tbl_document_versions ON tbl_document_versions.document_id = tbl_links.document_id
+	WHERE
+		tbl_views.document_version = tbl_document_versions.document_version
+	GROUP BY
+		tbl_views.view_seq,
+		tbl_views.link_id,
+		tbl_views.view_id,
+		tbl_views.viewed_at,
+		tbl_views.viewer,
+		tbl_views.document_version,
+		tbl_document_versions.page_count
+),
+links AS (
+	SELECT
+		tbl_links.link_seq,
+		tbl_links.link_id,
+		tbl_links.link_name,
+		tbl_links.created_at,
+		tbl_links.is_active,
+		tbl_links.document_id,
+		tbl_links.created_by,
+		tbl_links.is_email_required,
+		tbl_links.is_password_required,
+		tbl_links.is_verification_required,
+		tbl_links.is_domain_restricted,
+		tbl_links.is_download_allowed,
+		tbl_links.is_watermarked,
+		tbl_links.restricted_domains,
+		tbl_links.link_password,
+		coalesce(count(DISTINCT views.view_id), 0) AS view_count,
+		CASE WHEN count(DISTINCT views.view_id) = 0 THEN
+			ARRAY[]::json[]
+		ELSE
+			ARRAY (
+				SELECT
+					row_to_json(views.*)
+				FROM
+					views
+				WHERE
+					views.link_id = tbl_links.link_id
+				ORDER BY
+					views.view_seq DESC)
+		END AS views
+	FROM
+		tbl_links
+		LEFT JOIN views ON views.link_id = tbl_links.link_id
+	GROUP BY
+		tbl_links.link_seq,
+		tbl_links.link_id,
+		tbl_links.link_name,
+		tbl_links.created_at,
+		tbl_links.is_active,
+		tbl_links.document_id,
+		tbl_links.created_by,
+		tbl_links.is_email_required,
+		tbl_links.is_password_required,
+		tbl_links.is_verification_required,
+		tbl_links.is_domain_restricted,
+		tbl_links.is_download_allowed,
+		tbl_links.is_watermarked,
+		tbl_links.restricted_domains,
+		tbl_links.link_password
+	ORDER BY
+		tbl_links.link_seq DESC
+)
+SELECT
+	json_agg(row_to_json(t))
+FROM (
+	SELECT
+		tbl_documents.document_seq,
+		tbl_documents.document_id,
+		tbl_documents.created_at,
+		tbl_document_versions.created_at AS updated_at,
+		tbl_documents.document_name,
+		tbl_documents.source_path,
+		tbl_documents.source_type,
+		tbl_documents.created_by,
+		tbl_documents.org_id,
+		tbl_documents.is_enabled,
+		tbl_documents.image,
+		tbl_document_versions.document_version,
+		coalesce(count(links.link_id), 0) AS total_links_count,
+		coalesce(sum(links.view_count), 0) AS total_view_Count,
+		CASE WHEN count(DISTINCT links.link_id) = 0 THEN
+			ARRAY[]::json[]
+		ELSE
+			ARRAY (
+				SELECT
+					row_to_json(links.*)
+				FROM
+					links
+				WHERE
+					links.document_id = tbl_documents.document_id
+				ORDER BY
+					links.link_seq DESC)
+		END AS links
+	FROM
+		tbl_documents
+	LEFT JOIN links ON tbl_documents.document_id = links.document_id
+	LEFT JOIN tbl_document_versions ON tbl_documents.document_id = tbl_document_versions.document_id
+WHERE
+	tbl_document_versions.is_enabled = TRUE
+	AND CASE WHEN document_id_input IS NULL THEN
+		tbl_documents.org_id = list_org_from_user()
+	ELSE
+		tbl_documents.document_id = document_id_input
+	END
+GROUP BY
+	tbl_documents.document_seq,
+	tbl_documents.document_id,
+	tbl_documents.created_at,
+	tbl_document_versions.created_at,
+	tbl_documents.document_name,
+	tbl_documents.source_path,
+	tbl_documents.source_type,
+	tbl_documents.created_by,
+	tbl_documents.org_id,
+	tbl_documents.is_enabled,
+	tbl_documents.image,
+	tbl_document_versions.document_version
+ORDER BY
+	tbl_documents.document_seq DESC) t INTO return_data;
+	--
+	--
+	RETURN coalesce(return_data, '[]'::json);
+END;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.upsert_document(document_id_input text DEFAULT NULL::text, document_name_input text DEFAULT NULL::text, source_path_input text DEFAULT NULL::text, source_type_input text DEFAULT NULL::text)
+ RETURNS json
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+	found_document_id text;
+	insert_data tbl_document_versions;
+	return_data json;
+BEGIN
+	INSERT INTO tbl_documents(
+		document_id,
+		document_name,
+		source_path,
+		source_type)
+	VALUES (
+		COALESCE(document_id_input, gen_document_id()),
+		document_name_input,
+		source_path_input,
+		source_type_input)
+ON CONFLICT (
+	document_id)
+	DO UPDATE SET
+		source_path = EXCLUDED.source_path,
+		source_type = EXCLUDED.source_type
+	RETURNING
+		document_id INTO found_document_id;
+	--
+	-- if found_document_id is null, then throw an error
+	--
+	IF found_document_id IS NULL THEN
+		RAISE EXCEPTION 'Document not found';
+	END IF;
+	--
+	-- update tbl_document_versions
+	--
+	UPDATE
+		tbl_document_versions
+	SET
+		is_enabled = FALSE
+	WHERE
+		document_id = found_document_id;
+	--
+	-- insert new version
+	--
+	INSERT INTO tbl_document_versions(
+		document_id,
+		document_version,
+		is_enabled)
+	VALUES (
+		found_document_id,
+(
+			SELECT
+				coalesce(max(document_version), 0) + 1
+			FROM
+				tbl_document_versions
+			WHERE
+				document_id = found_document_id), TRUE)
+RETURNING
+	* INTO insert_data;
+	--
+	--
+	RETURN json_build_object('document_id', insert_data.document_id, 'document_version', insert_data.document_version);
+END;
+$function$
+;
+
+
