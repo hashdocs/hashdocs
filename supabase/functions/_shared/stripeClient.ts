5410894f15ed920c4de99043faf53db4a0c181dc
rbkayz <62215539+rbkayz@users.noreply.github.com>
2023-07-09 15:34:14 +0530

    fix: updated cd

--- supabase/functions/_shared/stripeClient.ts
+++ supabase/functions/_shared/stripeClient.ts
@@ -1,5 +1,5 @@
-import Stripe from "https://esm.sh/stripe?target=deno";
-import StripeType from "https://esm.sh/v128/stripe@12.12.0/types/index.d.ts";
+import Stripe from "https://esm.sh/stripe@12.12.0?target=deno";
+// import StripeType from "https://esm.sh/v128/stripe@12.12.0/types/index.d.ts";
 
 export const stripe = new Stripe(
     "sk_test_51NR4AQHUVtdp8pCHP9a1d4KLKL5UqY30ygCgZa4RfkfGJT9rnbAedgkD8gDoc50jXjKa33ngU3yNn5mippM2G7Hq00HsPfpT3t",
@@ -7,7 +7,7 @@ export const stripe = new Stripe(
       apiVersion: "2022-11-15",
       httpClient: Stripe.createFetchHttpClient(),
     }
-  ) as StripeType;
+  );
   
   // This is needed in order to use the Web Crypto API in Deno.
 export const cryptoProvider = Stripe.createSubtleCryptoProvider();
